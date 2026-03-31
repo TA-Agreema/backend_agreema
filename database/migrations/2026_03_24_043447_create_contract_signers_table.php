c9e662924c0c5fa583bae4e71b0ba946642edf3f
Anisa Fatimah Azzahra <anisazzahra26@student.uns.ac.id>
2026-03-31 14:59:58 +0700

    add field for contract signers table

---
+++ database/migrations/2026_03_24_043447_create_contract_signers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contract_signers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('contract_id');
+            $table->string('email')->nullable();
+            $table->foreignId('user_id')->nullable();
+            $table->enum('signer_type', ['internal', 'external']);
+            $table->integer('sequence')->default(1);
+            $table->enum('sign_status', ['pending', 'signed', 'declined'])->default('pending');
+            $table->enum('signature_type', ['digital', 'manual_upload'])->nullable();
+            $table->string('signature_path')->nullable();
+            $table->timestamp('signed_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contract_signers');
+    }
+};
